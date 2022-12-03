9d5745d1d4e9facd4fdfb98b8f1888a41a6b7099
Garrett Vernon <garv5@Garvs-PC.localdomain>
2022-12-03 13:36:08 -0700

    added Functions scripts

--- DatabaseFunctions.sql
+++ DatabaseFunctions.sql
@@ -546,4 +546,428 @@ end;
 $function$
 ;
 
-call seed_database(100);
+CREATE OR REPLACE FUNCTION base_subscription_revenue_per_dev(dev_id integer, month_year timestamp without time zone)
+ RETURNS money
+ LANGUAGE plpgsql
+AS $function$
+declare 
+	payPeriodUpperBound timestamp := date_trunc('month', (month_year));
+    payPeriodLowerBound timestamp := date_trunc('month', (month_year + interval '1 month'));
+   	devsum int;
+   	allGameCount float := 0; 
+   	devGameCount float := 0; 
+   	baseSubRev money := 0; 
+   	targetId int := dev_id;
+begin 
+		select count(g.id)
+		into devGameCount
+		from game as g inner join 
+		gameplay_record as gr on (g.id = gr.gameid and targetid = g.dev_id and (
+									gr.starttime > payPeriodUpperBound) and (
+									gr.starttime < payPeriodLowerBound ));
+--		raise notice 'devGameCount is %', devGameCount;							
+									
+	
+		select count(g.id)
+		into allGameCount
+		from game as g inner join 
+		gameplay_record as gr on ( g.id = gr.gameid
+									and gr.starttime > payPeriodUpperBound 
+									and (gr.starttime < payPeriodLowerBound )  );
+--		raise notice 'allGameCount is %', allGameCount;
+		
+	select sum(csph.amt)
+		into baseSubRev
+		from cust_sub cs inner join
+		cust_sub_pay_hist csph on (cs.id = csph.cust_sub_id
+										and (csph.pay_date > payPeriodUpperBound )
+										and (csph.pay_date < payPeriodLowerBound));
+					
+--					raise notice 'baseSubRev is %', baseSubRev;
+	if baseSubRev is null or allGameCount = 0 then 
+	return 0;
+	else 
+	return ((devGameCount/allGameCount) * (.1 * baseSubRev));
+	end if;
+end;
+$function$
+;
+
+CREATE OR REPLACE FUNCTION can_login(target_customer_id integer)
+ RETURNS boolean
+ LANGUAGE plpgsql
+AS $function$
+declare 
+	current_login_count int; 
+	num_allowed_logins int; 
+begin 
+	
+	select count(*) 
+	into current_login_count
+	from log_in_out_history lioh 
+	where ((lioh.success = true)
+			and (lioh.logout is null)
+			and (lioh.customerid = target_customer_id)
+		  );
+	
+	
+	select st.concurrentlogin
+	into num_allowed_logins
+	from cust_sub cs inner join
+	sub s on (cs.cust_id = target_customer_id  and cs.active = true and cs.sub_id = s.id) inner join 
+	sub_tier st on (st.id = s.tier_id);
+	
+	return (current_login_count < num_allowed_logins);
+end;
+
+$function$
+;
+
+CREATE OR REPLACE FUNCTION check_public(target_game_id integer)
+ RETURNS boolean
+ LANGUAGE plpgsql
+AS $function$
+declare 
+	public bool;
+begin 
+	
+	select g.public from game g where (g.id = target_game_id) into public;
+	return public;
+end;
+$function$
+;
+
+CREATE OR REPLACE FUNCTION feature_pack_revenue_per_dev(dev_id integer, month_year timestamp without time zone, fp_id integer)
+ RETURNS money
+ LANGUAGE plpgsql
+AS $function$
+declare 
+	payPeriodUpperBound timestamp := date_trunc('month', (month_year));
+    payPeriodLowerBound timestamp := date_trunc('month', (month_year + interval '1 month'));
+   	devsum int;
+   	allFPGameCount float := 0; 
+   	devFPGameCount float := 0; 
+   	FPSubRev money := 0; 
+   	targetId int := dev_id;
+   	game_rec record; 
+   	feature_rec record; 
+   	game_play_rev_count float; 
+  	target_fp_id int := fp_id;
+  	feature_play_rev_count float;
+begin 
+	for game_rec in 
+	(select distinct gr.id
+	from featurepack f  inner join 
+	game_feat g_f on((f.id = target_fp_id) and (g_f.feat_id = target_fp_id)) inner join 
+	game g on(g_f.game_id = g.id and (targetId = g.dev_id)) inner join 
+	gameplay_record gr on ((g.id = gr.gameid) and 
+							(gr.starttime > payPeriodUpperBound) and (
+							gr.starttime < payPeriodLowerBound )  ))
+	loop
+		select count(gfpr)
+		into game_play_rev_count
+			from game_feature_pack_rev gfpr
+			where game_rec.id = gfpr.game_record_id;
+		
+			raise notice 'GamePlayRevCount %', game_play_rev_count;
+		if game_play_rev_count >  1 then
+			devFPGameCount := devFPGameCount + 1/game_play_rev_count;
+			else 
+			devFPGameCount := devFPGameCount + 1;
+		end if;
+		raise notice 'DevFPGameCount % in loop', devFPGameCount;
+	end loop;
+	raise notice 'DevFPGameCount %', devFPGameCount;
+		
+
+	for feature_rec in 
+	(select distinct gr.id
+	from featurepack f  inner join 
+	game_feat g_f on((f.id = target_fp_id) and (g_f.feat_id = target_fp_id)) inner join 
+	game g on(g_f.game_id = g.id) inner join 
+	gameplay_record gr on ((g.id = gr.gameid) and 
+							(gr.starttime > payPeriodUpperBound) and (
+							gr.starttime < payPeriodLowerBound )  ))
+	loop 
+		select count(gfpr)
+		into feature_play_rev_count
+			from game_feature_pack_rev gfpr
+			where feature_rec.id = gfpr.game_record_id;
+		
+			raise notice 'GamePlayRevCount %', feature_play_rev_count;
+		if feature_play_rev_count >  1 then
+			allFPGameCount := allFPGameCount + 1/feature_play_rev_count;
+			else 
+			allFPGameCount := allFPGameCount + 1;
+		end if;
+		
+		
+	end loop;
+	
+	
+		raise notice 'allFPGameCount %', allFPGameCount;
+
+						
+						
+	select sum(csfph.amt)
+	into FPSubRev
+	from featurepack f inner join 
+	cust_sub_featurepk csf  on(f.id = target_fp_id and target_fp_id = csf.featpkid ) inner join 
+	cust_sub_feat_pay_hist csfph on(csf.id = csfph.cust_sub_feat_id and (
+							csfph.pay_date  > payPeriodUpperBound) and (
+							csfph.pay_date < payPeriodLowerBound ));
+						raise notice 'fpsubrev %', FPSubRev;
+		if allFPGameCount = 0 or FPSubRev is null then 
+		return 0; 
+		else 
+		return ((devFPGameCount/allFPGameCount) * (.1 * FPSubRev));
+		end if; 
+end;
+$function$
+;
+
+CREATE OR REPLACE PROCEDURE find_renewable()
+ LANGUAGE plpgsql
+AS $procedure$
+declare
+renew record;
+
+begin 
+	for renew in 
+	select id, cust_id, sub_id, current_term_exp, date_of_origin, autorenew, active
+	from cust_sub order by 4 desc
+	loop
+		if(renew.autorenew and (now() - renew.current_term_exp >= '1 second') and renew.active = true) then
+		 call renew_sub(renew);
+		end if;
+	end loop;
+
+end;$procedure$
+;
+
+
+CREATE OR REPLACE PROCEDURE find_renewable_fp()
+ LANGUAGE plpgsql
+AS $procedure$
+declare
+renew record;
+isactive bool;
+begin 
+	for renew in 
+	select id , cust_subid, featpkid, current_term_end, date_of_origin, autorenew 
+	from cust_sub_featurepk csf order by 4 desc loop
+		isactive = f.active from featurepack f where (renew.featpkid = f.id);
+		if(renew.autorenew and (now() - renew.current_term_end >= '1 second') and isactive = true) then
+		 call renew_sub_fp(renew);
+		end if;
+	end loop;
+
+end;$procedure$
+;
+
+
+CREATE OR REPLACE FUNCTION generate_base_sub_rev_report(month_year timestamp without time zone)
+ RETURNS TABLE(developer text, payout money, pay_month timestamp without time zone)
+ LANGUAGE plpgsql
+AS $function$
+declare 
+	all_devs record; 
+begin 
+	for all_devs in( 
+	select id, developername
+	from developer
+	)loop 
+		Developer := all_devs.developername;
+		payout := Base_Subscription_Revenue_Per_Dev(all_devs.id, month_year);
+		Pay_Month := date_trunc('month', (month_year));
+		return next;
+	end loop;
+end;
+$function$
+;
+
+CREATE OR REPLACE FUNCTION generate_feature_pack_sub_rev_report(month_year timestamp without time zone)
+ RETURNS TABLE(featurepack text, dev_name text, payout money, pay_month timestamp without time zone, acitve boolean)
+ LANGUAGE plpgsql
+AS $function$
+declare 
+	all_feature_packs record; 
+	all_devs record;
+begin 
+	for all_devs in( 
+	select id, developername
+	from developer
+	)
+	loop 
+		for all_feature_packs in(
+		select id, active, pack_name from featurepack
+		)
+		loop
+			dev_name := all_devs.developername;
+			FeaturePack := all_feature_packs.pack_name;
+			payout := Feature_Pack_Revenue_Per_Dev(all_devs.id,month_year , all_feature_packs.id);
+			Pay_Month := date_trunc('month', (month_year));
+			acitve := all_feature_packs.active;
+			return next;
+		end loop;
+	end loop;
+end;
+$function$
+;
+
+
+CREATE OR REPLACE PROCEDURE play_game(IN gameid integer, IN custid integer)
+ LANGUAGE plpgsql
+AS $procedure$
+declare 
+	cust_sub_id int; 
+	game_feat record; 
+	gameplay_record_id int;
+	can_play bool;
+	temp_record record;
+begin
+	select cs.cust_id 
+	into cust_sub_id
+	from customer c inner join 
+	cust_sub cs on(c.id = cs.cust_id)
+	where (cs.cust_id = custid );
+	 select game_playable(gameid, custid) into can_play;
+	
+	if can_play then 
+		insert into gameplay_record 
+		(cust_subid, gameid, starttime, duration)
+		values (cust_sub_id, gameid, now(), null)
+		returning id into gameplay_record_id;
+		
+		for temp_record in (
+		select gf.game_id , gf.feat_id 
+						from game_feat gf where (gf.game_id = gameid)
+		)loop
+			insert into game_feature_pack_rev 
+			(game_record_id, feature_pack_id) 
+			values (gameplay_record_id, temp_record.feat_id);
+		end loop;
+	else 
+	end if; 
+end; 
+$procedure$
+;
+
+
+CREATE OR REPLACE PROCEDURE renew_sub(IN mycustsub record)
+ LANGUAGE plpgsql
+AS $procedure$
+declare
+subprice money;
+submonths int;
+tempexp timestamp;
+tempmonths text;
+begin 
+	subprice = st.baseprice from cust_sub 
+	inner join sub on (mycustsub.sub_id = sub.id)
+	inner join sub_tier st on (st.id = sub.tier_id)
+	where (cust_sub.id = mycustsub.id);
+
+	submonths = s.numberofmonths from cust_sub
+	inner join sub s on (s.id = cust_sub.sub_id)
+	where (cust_sub.id = mycustsub.id);
+	tempmonths = submonths||' months';
+	tempexp = mycustsub.current_term_exp + tempmonths::interval;
+	tempexp = date_trunc('month', tempexp);
+	tempmonths = date_part('day', mycustsub.date_of_origin)||' days';
+	tempexp = tempexp + tempmonths::interval;
+	if(date_part('day', tempexp) < date_part('day', mycustsub.date_of_origin)) then
+	tempexp = date_trunc('month', tempexp);
+	tempexp = tempexp + date_part(
+        'days', 
+        (date_trunc('month', tempexp) + '1 month - 1 day'::interval)
+        );
+	end if;
+	if(mycustsub.autorenew = true) then
+	subprice = subprice * .85;
+	end if;
+	INSERT INTO cust_sub_pay_hist
+	(cust_sub_id, pay_date, amt, description)
+	VALUES(mycustsub.id, now(), subprice, 'renew subscription');
+		
+	UPDATE cust_sub
+	SET current_term_start= current_term_exp, current_term_exp= tempexp - '1 day'::interval
+	WHERE id=mycustsub.id;
+end;$procedure$
+;
+
+CREATE OR REPLACE PROCEDURE renew_sub_fp(IN mycustsub record)
+ LANGUAGE plpgsql
+AS $procedure$
+declare
+subprice money;
+submonths int;
+tempexp timestamp;
+tempmonths text;
+begin 
+	subprice = fp.baseprice from cust_sub_featurepk csf
+	inner join featurepack fp on (fp.id = csf.featpkid)
+	where (csf.id = mycustsub.id);
+
+	submonths = s.numberofmonths from cust_sub
+	inner join sub s on (s.id = cust_sub.sub_id)
+	where (cust_sub.id = mycustsub.id);
+
+	tempmonths = submonths||' months';
+	tempexp = mycustsub.current_term_end + tempmonths::interval;
+	tempexp = date_trunc('month', tempexp);
+	tempmonths = date_part('day', mycustsub.date_of_origin)||' days';
+	tempexp = tempexp + tempmonths::interval;
+
+	if(date_part('day', tempexp) < date_part('day', mycustsub.date_of_origin)) then
+	tempexp = date_trunc('month', tempexp);
+	tempexp = tempexp + date_part(
+        'days', 
+        (date_trunc('month', tempexp) + '1 month - 1 day'::interval)
+        );
+	end if;
+
+	if(mycustsub.autorenew = true) then
+	subprice = subprice - 1::money;
+	end if;
+	INSERT INTO cust_sub_feat_pay_hist
+	(cust_sub_feat_id, pay_date, amt, description)
+	VALUES(mycustsub.id, now(), subprice, 'renew subscription on featurepack');
+
+
+	UPDATE cust_sub_featurepk 
+	SET current_term_start= current_term_end, current_term_end = (tempexp - '1 day'::interval)
+	WHERE id=mycustsub.id;
+end;$procedure$
+;
+
+CREATE OR REPLACE PROCEDURE simulate_playing_games_random(IN number_of_plays_per_game integer DEFAULT 2)
+ LANGUAGE plpgsql
+AS $procedure$
+declare
+	customer_curs cursor for select c.id from customer c; 
+	cust_rec record; 
+	game_ record; 
+begin 
+	open customer_curs;
+	loop
+		fetch customer_curs into cust_rec;
+		exit when not found;
+		for game_ in (
+		select g.game_name, g.id from customer
+		inner join cust_sub cs on (customer.id = cs.cust_id and customer.id = 1)
+		left join cust_sub_featurepk csf on (cs.id = csf.cust_subid)
+		left join featurepack f on (csf.featpkid = f.id)
+		left join game_feat gf on (f.id = gf.feat_id)
+		left join game g on (gf.game_id = g.id) 
+		where (g.id is not null)
+		Union (select g.game_name , g.id from game g where (g.public = true))
+		)
+		loop
+			call play_game_random(game_.id, cust_rec.id);
+		end loop; 
+	end loop;
+	close customer_curs; 
+end;
+$procedure$
+;
